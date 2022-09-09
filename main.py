26a44d92d5df128913a6f9f859fcf28bcbf75540
hatler <HatlerRyan@gmail.com>
2022-09-08 22:07:37 -0600

    Only able to add a couple lines today.

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import random
 
 
 #----------------read the data-----------------#
-
+french_data = pd.read_csv("data/french_words.csv", )
+french_words_to_learn = french_data.to_dict(orient='records')
 new_word = {}
 def new_card():
     global new_word, flip_timer
     window.after_cancel(flip_timer)
     canvas.itemconfig(card_side, image=card_front)
-    french_data = pd.read_csv("data/french_words.csv", )
-    french_words_to_learn = french_data.to_dict(orient='records')
     new_word = random.choice(french_words_to_learn)
     next_french = new_word['French']
     canvas.itemconfig(language_title, text="French", fill="black")
     canvas.itemconfig(next_word_up, text=next_french, fill="black")
     flip_timer = window.after(3000, func=flip_card)
+    # print(new_word)
+
+def is_known():
+    french_words_to_learn.remove(new_word)
+    new_card()
+
 
 
 def flip_card():
@@ -24,6 +29,7 @@ def flip_card():
     canvas.itemconfig(language_title, text="English", fill="white")
     next_english = new_word['English']
     canvas.itemconfig(next_word_up, text=next_english, fill="white")
+
 
 
 
@@ -47,7 +53,7 @@ canvas.grid(column=0, row=0, columnspan=2)
 
 check_image = PhotoImage(file="images/right.png")
 x_image = PhotoImage(file="images/wrong.png")
-right_button = Button(image=check_image, background=BACKGROUND_COLOR, command=new_card)
+right_button = Button(image=check_image, background=BACKGROUND_COLOR, command=is_known)
 wrong_button = Button(image=x_image, background=BACKGROUND_COLOR, command=new_card)
 right_button.grid(column=0, row=2)
 wrong_button.grid(column=1, row=2)
